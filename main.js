d0731475b10a9954164b9f06b30626a7e9508730
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-12-11 13:32:56 -0600

    Added second cube made speed scale over time

--- main.js
+++ main.js
@@ -12,16 +12,22 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+const material2 = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const cube2 = new THREE.Mesh( geometry, material2 );
+scene.add( cube2 );
 
+camera.position.z = 5;
+var value = .001;
 function animate() {
 	requestAnimationFrame( animate );
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
-	//cube.translateX(.01);
-	cube.translateY(.01);
+	cube2.translateX(value);
+	cube.translateY(value);
+
+	value += .001;
 
 	renderer.render( scene, camera );
 }
