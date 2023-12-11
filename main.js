7227a80da432fcb707befd354f51930f29966674
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-12-11 16:39:12 -0600

    Back to standard

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ camera.position.z = 5;
 function animate() {
 	requestAnimationFrame( animate );
 
-	//cube.rotation.x += 0.01;
+	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
-	//cube2.rotation.z += 0.01;
+	cube2.rotation.z += 0.01;
 	cube2.rotation.x += 0.01;
 	
 	cube2.translateX(0.01);
