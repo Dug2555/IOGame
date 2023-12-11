be4daab580ab7df8b3d540aeac8399016fad7358
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-12-11 13:36:54 -0600

    Attempted Bug fix for second cube

--- main.js
+++ main.js
@@ -12,8 +12,9 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+const geometry2 = new THREE.BoxGeometry( 1, 1, 1 );
 const material2 = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const cube2 = new THREE.Mesh( geometry, material2 );
+const cube2 = new THREE.Mesh( geometry2, material2 );
 scene.add( cube2 );
 
 camera.position.z = 5;
@@ -24,6 +25,9 @@ function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
+	cube2.rotation.x += 0.01;
+	cube2.rotation.y += 0.01;
+	
 	cube2.translateX(value);
 	cube.translateY(value);
 
