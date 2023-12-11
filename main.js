d48fdd9d3ef837d964805c8ad9a867ddef3f481c
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-12-11 14:50:12 -0600

    Redeploy

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function animate() {
 	cube.rotation.y += 0.01;
 
 	cube2.rotation.y += 0.01;
-	cube2.rotation.z += 0.01;
+	cube2.rotation.x += 0.01;
 	
 	cube2.translateX(0.01);
 	cube.translateY(0.01);
