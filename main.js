a05b8055dc3a149a96453b1f673b1896f4ef5d71
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-12-11 16:53:34 -0600

    Switch rotation order for cube 2

--- main.js
+++ main.js
@@ -25,11 +25,13 @@ function animate() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 
-	cube2.rotation.z += 0.01;
+	cube.translateY(0.01);
+	
 	cube2.rotation.x += 0.01;
+	cube2.rotation.z += 0.01;
 	
 	cube2.translateX(0.01);
-	cube.translateY(0.01);
+	
 
 	renderer.render( scene, camera );
 }
