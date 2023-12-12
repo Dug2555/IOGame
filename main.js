3aee57ee8b58512edb4c97619864c0a370263f6b
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-12-11 21:23:52 -0600

    test bounce

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 0.1, 1 );
+const geometry = new THREE.BoxGeometry( 1, 1, 0.1 );
 const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 const puck = new THREE.Mesh( geometry, material );
 scene.add( puck );
@@ -23,10 +23,10 @@ function animate() {
 	puck.translateX(Xvalue);
 
 	var pos = puck.position;
-	if(puck.position[0] >= window.innerWidth || puck.position[0] <= 0){
+	if(puck.position[0] >= 5 || puck.position[0] <= 0){
 		Xvalue = Xvalue * -1;
 	}
-	if(puck.position[1] >= window.innerHeight || puck.position[1] <= 0){
+	if(puck.position[1] >= 5 || puck.position[1] <= 0){
 		Yvalue = Yvalue * -1;
 	}
 
