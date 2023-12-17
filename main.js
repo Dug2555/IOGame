f9e9e3499d48c8b64134d0311ce8e98df4f8e24b
Dug2555 <59838706+Dug2555@users.noreply.github.com>
2023-12-16 23:03:40 -0600

    reset to my code

--- main.js
+++ main.js
@@ -13,62 +13,16 @@ window.addEventListener('resize', function () {
      camera.updateProjectionMatrix();
 });
 
-const visibleHeightAtZDepth = (depth, camera) => {
-	// vertical fov in radians
-	const vFOV = camera.fov * Math.PI / 180;
-
-	// Math.abs to ensure the result is always positive
-	return 2 * Math.tan(vFOV / 2) * Math.abs(depth);
-};
-
-var mesh, controls;
-
-
-var screenDepth;
-screenDepth = findScreenDepth(camera, renderer)
-
-const redGeom =  new THREE.BoxGeometry( 5,5,5 );
-const redMat = new  THREE.MeshNormalMaterial({wireframe: true});
-const redMesh = new THREE.Mesh(redGeom, redMat);
-redMesh.position.set(0 /*px*/, 0, -screenDepth + 20);
-redMesh.scale.set(5,5,5)
-scene.add(redMesh, camera);
-
-function findScreenDepth(camera, renderer) {
-	const { near, far } = camera;
-	const { height: physicalViewHeight } = renderer.getDrawingBufferSize();
-	console.log(window.innerHeight, physicalViewHeight);
-	const threshold = 0.00000000000001;
-
-	return _findScreenDepth(near, far);
-
-	function _findScreenDepth(near, far) {
-
-		 const midpoint = (far - near) / 2 + near;
-		 const midpointHeight = visibleHeightAtZDepth(-midpoint, camera);
-
-		 if (Math.abs(physicalViewHeight / midpointHeight - 1) <= threshold)
-			  return midpoint;
-
-		 if (physicalViewHeight < midpointHeight)
-			  return _findScreenDepth(near, midpoint);else
-		 if (physicalViewHeight > midpointHeight)
-			  return _findScreenDepth(midpoint, far);else
-		 if (midpointHeight == physicalViewHeight) // almost never happens
-			  return midpoint;
-	}
-}
-
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( 1, 1, 1);
 const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
 const puck = new THREE.Mesh( geometry, material );
 scene.add( puck );
 
-camera.position.z = 20;
+camera.position.z = 5;
 var Xvalue = 0.03;
 var Yvalue = 0.01;
 function animate() {
@@ -81,10 +35,12 @@ function animate() {
 	var pos = puck.position;
 	console.log(pos.x);
 	console.log(window.innerWidth)
-	redMesh.position.x += ADD;
-     if (redMesh.position.x <= (-window.innerWidth/2)+20|| redMesh.position.x >= (window.innerWidth/2)-20) {
-          ADD *= -1;
-     }
+	if(pos.x <= (-window.innerWidth/2) + 5 || pos.x >= (window.innerwidth/2) - 5){
+		Xvalue = Xvalue * -1;
+	}
+	if(pos.y >= (window.innerHeight/2) - 5 || pos.y <= (-window.innerHeight/2) + 5){
+		Yvalue = Yvalue * -1;
+	}
 
 
 
